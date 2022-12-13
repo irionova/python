a07b2b7aa0a8fb7ef27692949eaf39fd30b245e3
irionova <117843454+irionova@users.noreply.github.com>
2022-12-13 14:14:56 +0300

    Update main.py

--- tenzor test/main.py
+++ tenzor test/main.py
@@ -1,9 +1,64 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
+
+
 
 URL_TESTING = 'https://tensor.ru/'
 TEXT_TESTING = "Тензор"
+BROWSER = "explorer" #можно заменить на explorer, firefox
+url="https://yandex.ru/images"
+
+def test_images():
+    driver = webdriver.Chrome()
+    driver.get("https://yandex.ru/")
+
+    """Нажимаем на поиск, чтобы открылась панель с сервисами"""
+    text = driver.find_element("class name", "dzen-search-arrow-common")
+    text.click()
+    time.sleep(2)
+
+    """Проверка наличия ссылки на картинки"""
+    try:
+        iframe = driver.find_element("css selector", 'iframe')
+        driver.switch_to.frame(iframe)
+        e=find_element(By.XPATH, "//a[@href='https://yandex.ru/images/?utm_source=main_stripe_big']")
+        print("ссылка на картинки на месте")
+        e.click()
+    except:
+        print("нет ссылки на картинки")
+
+    """Проверка верности ссылки"""
+    if driver.current_url.find(url) != (-1):
+        print("верная ссылка на картинки")
+    else:
+        print("не та ссылка")
+        driver.quit()
+
+
+    driver.quit()
+
+"""
+    try:
+        text = driver.find_element("class name", "dzen-search-arrow-common")
+        text.click()
+        time.sleep(2)
+        print("поле ввода на месте")
+
+        images_elem = driver.find_element(By.XPATH, "/html/body/form/div/div/div/div/ul/")
+
+        #images_elem = driver.find_elements("class name", "desktop-services_js_inited")
+        print(images_elem[0])
+        #images = driver.find_element("xpath", "//a[@href='https://yandex.ru/images/?utm_source=main_stripe_big/']")
+    except:
+        print("отсутствует поле ввода")
+"""
+
+
+
+
+
 
 
 def test_text():
@@ -51,4 +106,4 @@ def test_text():
 
 
 test_text ()
-
+test_images()
