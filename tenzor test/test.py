47d5e708802e4bb39809f2ae1f26e8bd0310aa5e
irionova <117843454+irionova@users.noreply.github.com>
2022-12-13 23:05:19 +0300

    master opening images

--- tenzor test/test.py
+++ tenzor test/test.py
@@ -5,16 +5,27 @@ from selenium.webdriver.common.by import By
 import time
 
 
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 
 # Navigate to Url
 
 driver.get("https://yandex.ru/")
 
 # Get all the elements available with tag name 'p'
-iframe = driver.find_element("css selector", 'iframe')
-driver.switch_to.frame(iframe)
-elements = driver.find_elements(By.TAG_NAME, 'a')
 
-for e in elements:
-    print(e.text)
+"""Нажимаем на поиск, чтобы открылась панель с сервисами"""
+text = driver.find_element("class name", "dzen-search-arrow-common")
+text.click()
+time.sleep(2)
+
+"""Проверка наличия ссылки на картинки"""
+try:
+    iframe = driver.find_element("css selector", 'iframe')
+    driver.switch_to.frame(iframe)
+    e = driver.find_element(By.XPATH, "//a[@href='https://yandex.ru/images/?utm_source=main_stripe_big']")#/span[@class='desktop-services__text']")
+    print("ссылка на картинки на месте")
+    e.click()
+except:
+    print("не кликается")
+
+time.sleep(2)
